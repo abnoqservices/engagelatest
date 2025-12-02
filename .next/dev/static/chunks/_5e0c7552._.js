e29b3e41dbcca0c878d81c5dae3fac1deec8e4b7
surendra <surendra@abnoq.com>
2025-12-02 11:43:02 +0530

    fixed the issue

--- .next/dev/static/chunks/_5e0c7552._.js
+++ .next/dev/static/chunks/_5e0c7552._.js
@@ -1638,7 +1638,6 @@ __turbopack_context__.s([
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$shared$2f$lib$2f$app$2d$dynamic$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/shared/lib/app-dynamic.js [app-client] (ecmascript)");
-"use client";
 ;
 ;
 const Template1 = ({ data })=>{
