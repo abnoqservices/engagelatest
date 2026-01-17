60a282f6202de37afe9aa219c0adf7707626b6a2
VAIBHAV SINGH <60459788+DevVaibhavSingh@users.noreply.github.com>
2026-01-17 13:13:04 +0530

    new

--- components/tempcomponent/index.ts
+++ components/tempcomponent/index.ts
@@ -11,7 +11,7 @@ export const SECTION_COMPONENTS: Record<string, any> = {
   image_gallery: dynamic(() => import("./image_gallery"), { ssr: true }),
   image_gallery_overlay: dynamic(() => import("./image_gallery_overlay"), { ssr: true }),
    image_gallery_coverflow: dynamic(() => import("./image_gallery_coverflow"), { ssr: true }),
-   description: dynamic(() => import("./Description"), { ssr: true }),
+   description: dynamic(() => import("./description"), { ssr: true }),
   product_header: dynamic(() => import("./product_header"), { ssr: true }),
   product_header_alt: dynamic(() => import("./product_header_alt"), { ssr: true }),
 
@@ -20,7 +20,7 @@ export const SECTION_COMPONENTS: Record<string, any> = {
   video: dynamic(() => import("./video"), { ssr: true }),
   // CTA: dynamic(() => import("./CTA"), { ssr: true }),
   social_links: dynamic(() => import("./social_links"), { ssr: true }),
-  contact: dynamic(() => import("./Contact"), { ssr: true }),
+  contact: dynamic(() => import("./contact"), { ssr: true }),
   testimonials: dynamic(() => import("./testimonials"), { ssr: true }),
   form: dynamic(() => import("./form"), { ssr: true }),
   form_popup: dynamic(() => import("./form_popup"), { ssr: true }),
